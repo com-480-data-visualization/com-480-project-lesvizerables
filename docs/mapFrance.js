465bc4b3b356319b7b885ea02e4fcc3be45351d4
Vera Fristedt <43444245+verafristedt@users.noreply.github.com>
2020-04-29 19:05:02 +0200

    Borders added and started on hover

--- docs/mapFrance.js
+++ docs/mapFrance.js
@@ -1,15 +1,49 @@
-var width = window.innerWidth,
-    height = window.innerHeight;
 
-var svg = d3.select(".mapdiv")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+const WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
+const OVERLAY_MULTIPLIER = 10;
+const OVERLAY_OFFSET = OVERLAY_MULTIPLIER / 2 - 0.5;
+
+function mouseOverHandler(d, i) {
+  d3.select(this).attr("fill", "black")
+}
+function mouseOutHandler(d, i) {
+  d3.select(this).attr("fill", "grey")
+}
+
+var svg = d3
+  .select(".mapdiv")
+  .append("svg")
+  .attr("width", WIDTH)
+  .attr("height", HEIGHT);
+
+/*// Overlay a transparent rectangle for pan and zoom.
+const g = svg.append("g");
+
+g
+  .append("rect")
+  .attr("width", WIDTH * OVERLAY_MULTIPLIER)
+  .attr("height", HEIGHT * OVERLAY_MULTIPLIER)
+  .attr(
+    "transform",
+    `translate(-${WIDTH * OVERLAY_OFFSET},-${HEIGHT * OVERLAY_OFFSET})`
+  )
+  .style("fill", "none")
+  .style("pointer-events", "all");*/
+
+const path = d3
+  .geoPath()
+  .projection(d3.geoMercator()
+    .center([4.6, 46.5])
+    .scale(2600)
+    .translate([WIDTH / 2, HEIGHT / 2]));
 
 d3.json("france.json").then(function(france) {
-	svg.append("path")
-		  .datum(topojson.feature(france, france.objects.poly))
-		  .attr("d", d3.geoPath().projection(d3.geoMercator().center([4.6, 46.5])
-      .scale(2600)
-      .translate([width / 2, height / 2])));
+	svg
+    .datum(topojson.feature(france, france.objects.poly))
+    .append("path")
+    .attr("d", path)
+    .attr("fill", "grey")
+    .style("stroke", "white")
+    .on("mouseover", mouseOverHandler)
+    .on("mouseout", mouseOutHandler);
 });
