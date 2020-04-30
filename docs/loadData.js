a9ce6bef23e3485de992e828e327f89199dbd7f0
ThomasBechMadsen <thomasbm@live.dk>
2020-04-30 21:14:07 +0200

    Wrangled data into nosql friendly format and almost set my computer on fire in the process

--- docs/loadData.js
+++ docs/loadData.js
@@ -1,5 +1,5 @@
 const dbRef = firebase.database().ref()
 
-dbRef.child(0).once('value').then(function (snapshot) {
+dbRef.once('value').then(function (snapshot) {
     console.log(snapshot.val())
 });
