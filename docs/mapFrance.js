6d148dfe57ebba916910c7c300b7adb198d9ef3a
Vera Fristedt <43444245+verafristedt@users.noreply.github.com>
2020-04-30 17:19:45 +0200

    Mapped province IDs to names

--- docs/mapFrance.js
+++ docs/mapFrance.js
@@ -4,31 +4,45 @@ const OVERLAY_MULTIPLIER = 10;
 const OVERLAY_OFFSET = OVERLAY_MULTIPLIER / 2 - 0.5;
 
 function mouseOverHandler(d, i) {
-  d3.select(this).attr("fill", "black")
+  d3.select(this).attr("fill", "grey");
+
+  console.log(`translate(${path.centroid(d)})`);
+
+  //Testing different ways of trying to get the text to show when hovering
+/*
+  var div = document.createElement("div");   // Create a <button> element
+  div.innerHTML = d.properties.ID;
+  div.id = "::province";
+  div.style.cssText = 'transform: {`translate(${path.centroid(d)})`}; width:200px;height:200px;';
+                 // Insert text
+  document.body.appendChild(div);*/
+
+  /*.selectAll("text")
+  .data(d)
+  .enter()
+  .append("text")
+  .text(function(d) {
+    console.log(d.properties.ID)
+    if(mapObj[d.properties.ID] == null)
+      return d.properties.ID;
+    else
+      return mapObj[d.properties.ID];
+  })
+  .attr("transform", d => `translate(${path.centroid(d)})`)
+  .attr("text-anchor", "middle")
+  .attr("font-size", 10)
+  .attr("class","labels");*/
 }
 function mouseOutHandler(d, i) {
-  d3.select(this).attr("fill", "grey")
+  d3.select(this).attr("fill", "white")
 }
 
 var svg = d3
   .select(".mapdiv")
+  .attr("id", "svg")
   .append("svg")
   .attr("width", WIDTH)
   .attr("height", HEIGHT);
-
-/*// Overlay a transparent rectangle for pan and zoom.
-const g = svg.append("g");
-
-g
-  .append("rect")
-  .attr("width", WIDTH * OVERLAY_MULTIPLIER)
-  .attr("height", HEIGHT * OVERLAY_MULTIPLIER)
-  .attr(
-    "transform",
-    `translate(-${WIDTH * OVERLAY_OFFSET},-${HEIGHT * OVERLAY_OFFSET})`
-  )
-  .style("fill", "none")
-  .style("pointer-events", "all");*/
 
 const path = d3
   .geoPath()
@@ -39,14 +53,56 @@ const path = d3
 
 var g = svg.append("g");
 
+var mapObj = {
+  FR42: "Alsace",
+  FR61: "Aquitaine",
+  FR72: "Auvergne",
+  FR25: "Basse-Normandie",
+  FR26: "Bourgogne",
+  FR52: "Bretagne",
+  FR24: "Centre",
+  FR21: "Champagne-Ardenne",
+  FR83: "Corse",
+  FR43: "Franche-Comté",
+  FR23: "Haute-Normandie",
+  FR10: "Ile-de-France",
+  FR81: "Languedoc-Roussillon",
+  FR63: "Limousin",
+  FR41: "Lorraine",
+  FR62: "Midi-Pyrénées",
+  FR30: "Nord-Pas-de-Calais",
+  FR51: "Pays-de-la-Loire",
+  FR22: "Picardie",
+  FR53: "Poitou-Charantes",
+  FR82: "Provence-Alpes-Côtes d'Azur",
+  FR71: "Rhône-Alpes"
+};
+
 d3.json("france.json").then(function(france) {
 	g.selectAll("path")
     .data(topojson.feature(france, france.objects.poly).features)
     .enter()
     .append("path")
     .attr("d", path)
-    .attr("fill", "grey")
-    .style("stroke", "white")
+    .attr("fill", "white")
+    .style("stroke", "black")
     .on("mouseover", mouseOverHandler)
     .on("mouseout", mouseOutHandler);
+
+/* // This writes out all the country names on the map
+  g.selectAll("text")
+    .data(topojson.feature(france, france.objects.poly).features)
+    .enter()
+    .append("text")
+    .text(function(d) {
+      console.log(d.properties.ID)
+      if(mapObj[d.properties.ID] == null)
+        return d.properties.ID;
+      else
+        return mapObj[d.properties.ID];
+    })
+    .attr("transform", d => `translate(${path.centroid(d)})`)
+    .attr("text-anchor", "middle")
+    .attr("font-size", 10)
+    .attr("class","labels");*/
 });
