c5b4cd3da1d18382b67e85319c4b2f5c0dc95756
marisofie <marilerfaldet@gmail.com>
2020-05-23 18:39:00 +0200

    fixed hamburger menu, implemented on all pages

--- docs/hammenu.js
+++ docs/hammenu.js
@@ -1,5 +1,14 @@
 
 function toggleClass() {
   let menu = document.querySelector(".ham-menu");
-  menu.classList.toggle("toggleCls");
+  menu.classList.toggle("toggleMenu");
+
+  let icon = document.querySelector(".nav-icon");
+  icon.classList.toggle("toggleIcon")
+
 }
+
+
+//$("#nav-icon").click(function()) {
+//  $("#nav-icon").addClass("nav-opened");
+//}
