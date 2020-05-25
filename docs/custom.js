98eb1c3ef635baad77183f149f03f48b7b156a4a
marisofie <marilerfaldet@gmail.com>
2020-05-25 10:29:53 +0200

    changed filename and fixed dependency

---
+++ docs/custom.js
@@ -0,0 +1,27 @@
+
+function toggleClass() {
+  let menu = document.querySelector(".ham-menu");
+  menu.classList.toggle("toggleMenu");
+
+  let icon = document.querySelector(".nav-icon");
+  icon.classList.toggle("toggleIcon")
+
+}
+
+function openDropdown() {
+  document.getElementById("searchDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
