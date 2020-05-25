487f38adf9c539d20dcf1b6642a4d3414cb9b549
marisofie <marilerfaldet@gmail.com>
2020-05-25 12:07:10 +0200

    added layout for search fields

--- docs/custom.js
+++ docs/custom.js
@@ -1,27 +1,44 @@
 
-function toggleClass() {
+function toggleHammenu() {
   let menu = document.querySelector(".ham-menu");
   menu.classList.toggle("toggleMenu");
 
   let icon = document.querySelector(".nav-icon");
-  icon.classList.toggle("toggleIcon")
-
+  icon.classList.toggle("toggleIcon");
 }
 
-function openDropdown() {
-  document.getElementById("searchDropdown").classList.toggle("show");
+function toggleDropdown(a) {
+  a.parentNode.getElementsByClassName("dropdown-content")[0].classList.toggle("show");
+    //document.getElementById("searchProvince").classList.toggle(".drop-province show");
+    //document.getElementById("searchRegion").classList.toggle(".drop-region show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+  if (!event.target.matches('.dropfield')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
       }
+    }
+  }
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myinput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("dropfield");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
     }
   }
 }
