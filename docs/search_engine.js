8237fc05bf021c05da22c9755493307f84104c20
Vera Fristedt <43444245+verafristedt@users.noreply.github.com>
2020-05-26 10:01:39 +0200

    Variety click navigates to search page

--- docs/search_engine.js
+++ docs/search_engine.js
@@ -1,6 +1,27 @@
 
 const dbRef = firebase.database().ref();
 
+function getUrlVars() {
+    var vars = [], hash;
+    // Get arguments from url
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    // Check if there were arguments
+    if (hashes.includes(window.location.href)) return null;
+    else {
+      for(var i = 0; i < hashes.length; i++) {
+          hash = hashes[i].split('=');
+          vars.push(hash[0]);
+          vars[hash[0]] = hash[1];
+      }
+      console.log(vars);
+      console.log('hej');
+      // Return arguments
+      return vars;
+    }
+};
+
+var allVars = getUrlVars();
+
 // Sets the options of the dropdown menu and once an alternative is selected, the text field is changed
 function edit_value(event) {
   document.getElementById("prov_input").value = this.innerText;
