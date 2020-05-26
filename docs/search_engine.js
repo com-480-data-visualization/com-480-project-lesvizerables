f863e7a2870d50bec618c5968a2f740c5900f122
Vera Fristedt <43444245+verafristedt@users.noreply.github.com>
2020-05-26 11:28:15 +0200

    Search engine change if nav from variety page

--- docs/search_engine.js
+++ docs/search_engine.js
@@ -1,9 +1,9 @@
 
 const dbRef = firebase.database().ref();
 
+// Function to get arguments from url
 function getUrlVars() {
     var vars = [], hash;
-    // Get arguments from url
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
     // Check if there were arguments
     if (hashes.includes(window.location.href)) return null;
@@ -19,34 +19,43 @@ function getUrlVars() {
 };
 
 var allVars = getUrlVars();
-
-//Extract the variety if there exists one
-if(allVars) {
-  var variety = "";
-  // Handle if variety includes several words
-  var hashes = allVars.variety.split('%20');
-  for(var i = 0; i < hashes.length; i++) {
-    variety += hashes[i];
-    if(i < hashes.length) variety += " ";
+function load_search_engine(){
+  //Extract the variety if there exists one
+  if(allVars) {
+    var variety = "";
+    // Handle if variety includes several words
+    var hashes = allVars.variety.split('%20');
+    for(var i = 0; i < hashes.length; i++) {
+      variety += hashes[i];
+      if(i < hashes.length) variety += " ";
+    }
+    // Add variety to search query and show results
+    document.getElementById("var_input").value = variety;
+    document.getElementById("var_input").style = "color:black;"
   }
-  // Add variety to search query and show results
-}
-else {
-  // Otherwise show search engine without any input
-}
+  // Load the dropdown values
+  load_dropdowns();
 
-// Sets the options of the dropdown menu and once an alternative is selected, the text field is changed
-function edit_value(event) {
+};
+
+// Loading the dropdown alternatives
+function load_dropdowns() {
+
+  // Sets the options of the province dropdown menu
+  dbRef.child('province_names').once('value').then(function (snapshot) {
+    dataCache = snapshot.val();
+
+    for (obj in dataCache) {
+      var newcontent = document.createElement('div');
+      newcontent.innerHTML = "<a class='dropdown_ele' onclick='javascript:edit_prov_val.call(this, event);'>"+ dataCache[obj] + "</a>";
+      document.getElementById("province_dropfield").appendChild(newcontent.firstChild);
+    }
+  });
+
+};
+
+//Change province text field when alternative is selected
+function edit_prov_val(event) {
   document.getElementById("prov_input").value = this.innerText;
   document.getElementById("prov_input").style = "color:black;"
 };
-
-dbRef.child('province_names').once('value').then(function (snapshot) {
-  dataCache = snapshot.val();
-
-  for (obj in dataCache) {
-    var newcontent = document.createElement('div');
-    newcontent.innerHTML = "<a class='dropdown_ele' onclick='javascript:edit_value.call(this, event);'>"+ dataCache[obj] + "</a>";
-    document.getElementById("province_dropfield").appendChild(newcontent.firstChild);
-  }
-});
