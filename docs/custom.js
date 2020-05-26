afcbebb374b7b14b4a496f57d5a8d2c0483bf4ae
marisofie <marilerfaldet@gmail.com>
2020-05-26 09:35:44 +0200

    added filtering of search fields

--- docs/custom.js
+++ docs/custom.js
@@ -9,8 +9,6 @@ function toggleHammenu() {
 
 function toggleDropdown(a) {
   a.parentNode.getElementsByClassName("dropdown-content")[0].classList.toggle("show");
-    //document.getElementById("searchProvince").classList.toggle(".drop-province show");
-    //document.getElementById("searchRegion").classList.toggle(".drop-region show");
 }
 
 window.onclick = function(event) {
@@ -27,11 +25,43 @@ window.onclick = function(event) {
   }
 }
 
-function filterFunction() {
+function filterProvince() {
   var input, filter, ul, li, a, i;
-  input = document.getElementById("myinput");
+  input = document.getElementById("prov_input");
   filter = input.value.toUpperCase();
-  div = document.getElementById("dropfield");
+  div = document.getElementById("province_dropfield");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+function filterRegion() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("reg_input");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("region_dropfield");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+function filterVariety() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("var_input");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("variety_dropfield");
   a = div.getElementsByTagName("a");
   for (i = 0; i < a.length; i++) {
     txtValue = a[i].textContent || a[i].innerText;
